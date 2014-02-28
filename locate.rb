06f68f8b1c73759506a66d1a782857ab5bc765a8
Furqan Ahmed <fam4792@gmail.com>
2014-02-28 23:29:54 +0500

    Inclusion of nextl function

--- locate.rb
+++ locate.rb
@@ -1,22 +1,29 @@
 require "geocoder"
 
-p"Input your location"
+
+def nextl
+		print "\n"
+	end
+
+
+print "Input your location : "
 myinp = gets.chomp
 
 if myinp.include? "."
 
 	coord = Geocoder.search("myinp").first.address
+	nextl
 	print "Address for your entered Coordinates : "
 	print coord
-	print "" 
+	nextl 
 
 
 
 else
 	coord = Geocoder.search("myinp").first.coordinates
+	nextl
 	print "Coordinates for your entered address : Latitude = #{coord.first} , Longitude = #{coord.last}"
-	print  ""
-
-
-
+	nextl
 end
+
+nextl
