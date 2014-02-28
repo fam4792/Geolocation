d234ab96fc62778636f6466d5caa4cd9f709872c
Furqan Ahmed <fam4792@gmail.com>
2014-02-28 23:45:02 +0500

    Proper functionality acheived

--- locate.rb
+++ locate.rb
@@ -6,24 +6,25 @@ def nextl
 	end
 
 
+
 print "Input your location : "
 myinp = gets.chomp
 
+
+
 if myinp.include? "."
 
-	coord = Geocoder.search("myinp").first.address
+	coord = Geocoder.search(myinp).first.address
 	nextl
 	print "Address for your entered Coordinates : "
 	print coord
 	nextl 
-
-
-
 else
-	coord = Geocoder.search("myinp").first.coordinates
+	coord = Geocoder.search(myinp).first.coordinates
 	nextl
 	print "Coordinates for your entered address : Latitude = #{coord.first} , Longitude = #{coord.last}"
 	nextl
 end
+
 
 nextl
